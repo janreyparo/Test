182423f79c00026f6f3c19f643335c7dea69e4ea
janreyparo <162281652+janreyparo@users.noreply.github.com>
2024-03-05 19:52:32 +0800

    Add files via upload

---
+++ python day.py
@@ -0,0 +1,7 @@
+name = input("what is your name?:")
+age = int(input("what is your age?:"))
+height =int(input("what is your height?:"))
+
+#print("Hello: "+name)
+print("Your are "+str(age)+" years old")
+print("Your height is: "+str(height)+"cm tall")
